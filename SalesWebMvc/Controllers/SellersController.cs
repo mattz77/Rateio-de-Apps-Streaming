efd7fe609454954753ad6fcae00720d0c535de57
Mateus <mattz77.mo@gmail.com>
2020-03-09 11:50:46 -0300

    Tela Simples de Cadastro

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Services;
 using SalesWebMvc.Controllers;
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
 {
@@ -27,5 +28,19 @@ namespace SalesWebMvc.Controllers
             return View(list);
 
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
