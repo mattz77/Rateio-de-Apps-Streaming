0b5203c6870614ab7a355ac600e40e490aa1a142
Mateus <mattz77.mo@gmail.com>
2020-03-10 15:43:36 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,24 +8,32 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+        
+        [Required(ErrorMessage ="{0} Required")]
+        [StringLength(60,MinimumLength = 3,ErrorMessage ="{0} size should be between {2} and {1}")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} Required")]
         [DataType(DataType.EmailAddress)]
-        
+        [EmailAddress(ErrorMessage ="Enter a valid Email")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} Required")]
         [Display(Name = "Birth Date")]
-        [DataType(DataType.Date)]      
-        
+        [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
        
+        
+        [Required(ErrorMessage = "{0} Required")]
+        [Range(100.0, 5000.0, ErrorMessage = "{0} must be from{1} to {2}")]
         [Display(Name="Base Salary")]
         [DisplayFormat(DataFormatString ="{0:f2}")]
         public double BaseSalary { get; set; }
         
         
         public Department Department { get; set; }
-        
-        
         public int DepartmentId { get; set; }
         
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
@@ -60,6 +68,8 @@ namespace SalesWebMvc.Models
         {
             return Sales.Where(sr => sr.Date >= Initial && sr.Date <= Final).Sum(sr => sr.Amount);
         }
+
+
 
     }
 }
